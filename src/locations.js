3c782bdf789e48d20e867987cad0cdeb263b4f66
covs67 <43531537+covs67@users.noreply.github.com>
2019-12-02 15:06:28 +0000

    locations + index.css

--- src/locations.js
+++ src/locations.js
@@ -21,36 +21,31 @@ const loadPlaces = function (coords) {
     return PLACES;
 };
 
-// getting places from REST APIs
-function loadPlaceFromAPIs(position) {
-    const params = {
-        radius: 300,    // search places not farther than this value (in meters)
-        clientId: 'HZIJGI4COHQ4AI45QXKCDFJWFJ1SFHYDFCCWKPIJDWHLVQVZ',
-        clientSecret: 'GYRKWWJMO2WK3KIRWBXIN5FQAWXTVFIK2QM4VQWNQ4TRAKWH',
-        version: '20300101',    // foursquare versioning, required but unuseful for this demo
-    };
-
-    // CORS Proxy to avoid CORS problems
-    const corsProxy = 'https://cors-anywhere.herokuapp.com/';
-
-    // Foursquare API
-    const endpoint = `${corsProxy}https://api.foursquare.com/v2/venues/search?intent=checkin
-        &ll=${50.37433},${-4.13906}
-        &radius=${params.radius}
-        &client_id=${params.clientId}
-        &client_secret=${params.clientSecret}
-        &limit=15
-        &v=${params.version}`;
-    return fetch(endpoint)
-        .then((res) => {
-            return res.json()
-                .then((resp) => {
-                    return resp.response.venues;
-                })
+// getting places from the database
+async function loadPlaceFromAPIs(position) {
+    let result = []
+    //set loadMode to 'server' to use service call
+    let loadMode ='server'
+    if(loadMode ==='serverless'){
+        const locationsRef = database.ref('locations');
+        const locations = await locationsRef.once('value');
+        locations.forEach(function(snapshot) {
+            result.push({
+                name:snapshot.val().name,
+                desciption:snapshot.val().description?snapshot.val().description:'Empty Description',
+                location: {
+                    lat:snapshot.val().lat,
+                    lng:snapshot.val().lng,
+                }
+            });
         })
-        .catch((err) => {
-            console.error('Error with places API', err);
-        })
+    } else if (loadMode ==='server'){
+        result = await fetch('/locations').then(
+           
+            res => res.json()
+        )
+    }
+    return result
 };
 
 
@@ -60,18 +55,17 @@ window.onload = () => {
     // first get current user location
     return navigator.geolocation.getCurrentPosition(function (position) {
 
-        // than use it to load from remote APIs some places nearby
+        // then use it to load from remote APIs some places nearby
         loadPlaces(position.coords)
             .then((places) => {
                 places.forEach((place) => {
-                    const latitude =  50.37433;
-                    const longitude = -4.13906;
+                    const latitude =  place.location.lat;
+                    const longitude = place.location.lng;
 
                     // add place name
                     const text = document.createElement('a-link');
-                    text.setAttribute('gps-entity-place', `latitude: ${50.37433}; longitude: ${-4.13906};`);
-                    text.setAttribute('title', 'RLB 201');
-                    text.setAttribute('href', 'http://www.example.com/');
+                    text.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+                    text.setAttribute('title', place.type+':'+ place.name+': '+place.desciption);
                     text.setAttribute('scale', '13 13 13');
 
                     text.addEventListener('loaded', () => {
