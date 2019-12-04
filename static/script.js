07eea15c6b5882b4c8f1875e0a3db1ab851b1869
covs67 <43531537+covs67@users.noreply.github.com>
2019-12-04 12:33:13 +0000

    places

--- static/script.js
+++ static/script.js
@@ -67,7 +67,7 @@ window.onload = () => {
                     // add place name
                     const text = document.createElement('a-link');
                     text.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-                    text.setAttribute('title', place.type+':'+ place.name+': '+place.description);
+                    text.setAttribute('title', +place.name+': '+place.description);
                     text.setAttribute('scale', '13 13 13');
 
                     text.addEventListener('loaded', () => {
